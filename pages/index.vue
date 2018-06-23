e7ff4a3da76b3ff31adbc1d65fa583d139c18c73
Jonas <jonasparnow@gmail.com>
2018-06-23 15:28:44 +0200

    insert links

--- pages/index.vue
+++ pages/index.vue
@@ -15,20 +15,20 @@
       <h4>Links</h4>
       <p>As my work and the projects I work on cover different diciplines and topics I like to keep them in seperate places:</p>
       <ul class="list">
-        <li><a href="#">visualising.systems</a><br />My recent data visualisation and information design works</li>
-        <li><a href="#">away.photos</a><br />A collection of my favourite photography projects</li>
-        <li><a href="#">karat.studio</a><br />The interface and information studio I co-run</p></li>
-        <li><a href="#">datavis.berlin</a><br />A collection of data visualisation links I build for the data vis Meetup</li>
-        <li><a href="#">zeto.io</a><br />Some online tools I build over the last years</li>
+        <li><a href="https://visualising.systems">visualising.systems</a><br />My recent data visualisation and information design works</li>
+        <li><a href="https://away.photos">away.photos</a><br />A collection of my favourite photography projects</li>
+        <li><a href="http://karat.studio">karat.studio</a><br />The interface and information studio I co-run</p></li>
+        <li><a href="https://datavis.berlin">datavis.berlin</a><br />A collection of data visualisation links I build for the data vis Meetup</li>
+        <li><a href="http://zeto.io">zeto.io</a><br />Some online tools I build over the last years</li>
       </ul>
     </section>
     <section>
       <h4>Vita</h4>
-      <p>Jonas Parnow is a designer for information and data. After his media design Bachelor of Arts from the <a href="">RFH Cologne</a> in 2012 he started at the infographic agency <a href="">Infographics Group</a>. Besides that, he finished his design Master of Arts at the <a href="">FH Potsdam</a>. Since 2017, he works as a part-time research associate at the <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a> and works in a EU-funded research project on the visualisation of climate change scenarios. He is also working at the interface and information design <a href="http://karat.studio">Studio Karat</a> that he co-founded. He also co-organises the <a href="https://datavis.berlin">data visualisation meetup</a> in Berlin.</p>
+      <p>Jonas Parnow is a designer for information and data. After his media design Bachelor of Arts from the <a href="https://www.rfh-koeln.de/">RFH Cologne</a> in 2012 he started at the infographic agency <a href="https://infographics.group/">Infographics Group</a>. Besides that, he finished his design Master of Arts at the <a href="https://www.fh-potsdam.de/studieren/fachbereiche/design/">FH Potsdam</a>. Since 2017, he works as a part-time research associate at the <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a> and works in a EU-funded research project on the visualisation of climate change scenarios. He is also working at the interface and information design <a href="http://karat.studio">Studio Karat</a> that he co-founded. He also co-organises the <a href="https://datavis.berlin">data visualisation meetup</a> in Berlin.</p>
     </section>
     <section lang="de">
       <h4>Lebenslauf</h4>
-      <p>Jonas Parnow ist Designer für Informationen und Daten. Nach dem Mediendesign Bachelor an der <a href="">RFH Köln</a> hat er 2012 bei der Infografik-Agentur <a href="">Infographics Group</a> angefangen. Parallel zu seiner Arbeit dort hat er 2015 an der <a href="">FH Potsdam</a> einen Master in Interface Design abgeschlossen. Seit 2017 arbeitet er als wissenschaftlicher Mitarbeiter im <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a> und beschäftigt sich dort in einem EU-weiten Forschungsprojekt mit der Visualisierung von Klimawandel-Szenarien. Schon während seiner Arbeit in der Agentur hat er <a href="http://karat.studio">Studio Karat</a> gegründet – ein Interface- und Informationsdesign-Studio. Er co-organisiert außerdem das <a href="https://datavis.berlin">Daten Visualisierung Meetup</a> in Berlin.</p>
+      <p>Jonas Parnow ist Designer für Informationen und Daten. Nach dem Mediendesign Bachelor an der <a href="https://www.rfh-koeln.de/">RFH Köln</a> hat er 2012 bei der Infografik-Agentur <a href="https://infographics.group/">Infographics Group</a> angefangen. Parallel zu seiner Arbeit dort hat er 2015 an der <a href="https://www.fh-potsdam.de/studieren/fachbereiche/design/">FH Potsdam</a> einen Master in Interface Design abgeschlossen. Seit 2017 arbeitet er als wissenschaftlicher Mitarbeiter im <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a> und beschäftigt sich dort in einem EU-weiten Forschungsprojekt mit der Visualisierung von Klimawandel-Szenarien. Schon während seiner Arbeit in der Agentur hat er <a href="http://karat.studio">Studio Karat</a> gegründet – ein Interface- und Informationsdesign-Studio. Er co-organisiert außerdem das <a href="https://datavis.berlin">Daten Visualisierung Meetup</a> in Berlin.</p>
     </section>
   </div>
 </template>
@@ -46,12 +46,6 @@
       ...mapState([
         'header'
       ])
-    },
-    methods: {
-    },
-    directives: {
-    },
-    components: {
     }
   }
 </script>
