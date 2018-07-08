dc7f1dc6ac1fd6b4f490e9bbe06b201c9da28ed9
Jonas <jonasparnow@gmail.com>
2018-07-08 15:40:33 +0200

    clean up

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,20 +11,12 @@
 </template>
 
 <script>
-  // import { mapState } from 'vuex'
-  // import { seo } from '~/assets/js/seo.js'
   import Header from '~/components/Header.vue'
   import Navigation from '~/components/Navigation.vue'
   import Aside from '~/components/Aside.vue'
   import Footer from '~/components/Footer.vue'
 
   export default {
-    computed: {
-    },
-    methods: {
-    },
-    directives: {
-    },
     components: {
       Header,
       Navigation,
