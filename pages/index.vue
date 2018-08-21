936fdaed726e24401c231fa9b181a3d7ac894622
Jonas <jonasparnow@gmail.com>
2018-08-21 22:47:06 +0200

    german text added

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="container rows">
-    <header>
+    <header v-if="process.env.lang === 'de'">
+      <h3>Jonas Parnow arbeitet an der <a href="http://senses-project.org/">Visualisierung von Klima Wandel Szenarien</a> als wissenschaftlicher Mitarbeiteiter im <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a>. Er hat das Interface- und Informationsdesign <a href="http://karat.studio">Studio Karat</a> mitgegründet und arbeitet als Freelancer in Berlin.</h3>
+    </header>
+    <header v-else>
       <h3>Jonas Parnow works on the <a href="http://senses-project.org/">visualisation of climate change scenarios</a> as a research associate at the <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a>. He&nbsp;co-runs the interface and information design <a href="http://karat.studio">Studio Karat</a> and works as a freelancer in Berlin.</h3>
     </header>
-    <section>
+    <section v-if="process.env.lang === 'de'">
+      <h4>Einleitung</h4>
+      <p>Ich interessiere mich für den Prozess der Umwand&shy;lung von Infor&shy;mationen und Daten in <em>visuelle Strukturen</em>. Ich habe ein kritisches und weites Verständnis von dem, was Visualisierungen und Interface sind und was sie für eine Bedeutung und Einfluss auf unsere Gesellschaft haben. Andere Themen sind <em>post-digitale Medien</em>, <em>Privatsspähre</em> und <em>Klima Wandel</em>.</p>
+      <p>Ich bevorzuge es für Organisationen, Stiftungen, Universitäten, Journalisten und Aktivisten mit einer Open Source Lizenz zu arbeiten. Ich gebe außerdem Vorträge und unterrichte an Fachhochschulen. In Berlin co-organisiere ich außerdem das <a href="https://datavis.berlin">Daten Visualisierung Meetup</a>.</p>
+      <p>Ich bin vertraut mit geläufigen Front-end und ein paar Back-end Programmiersprachen sowie einer Reihe von Grapfik-Anwendungen. Die meisten meiner Visualisierungen entstehen aus einer Kombination von unterschiedlichsten Tools, Anwendungen und eigener Programmierung.</p>
+      <p>Mein privates Leben und meine Arbeit beeinflussen und bedingen sich gegenseitig. So interessiere ich mich für alle möglichen Formen von <em>Design</em>, <em>Kultur</em> und <em>Technology</em>. Besonders unter dem Aspekt, wie wir diese Techniken nutzen können, um eine diverse, gleichberechtigte und faire Welt schaffen können.</p>
+      <p>Obwohl Fotografie mit meiner Arbeit verbunden ist, stellt sie eher eine Pause und Erhohlung da. Das Einfangen von Schönheit ist eine wunderbare Kombination meiner Liebe zu Ästhetik und meine Neugir nach fremden Orten.</p>
+    </section>
+    <section v-else>
       <h4>Introduction</h4>
       <p>I’m interested in trans­­forming data and in&shy;for&shy;ma&shy;tion into <em>visual struc­tures</em>. I have a critical and open under­standing of what vi&shy;su&shy;al&shy;isa&shy;tion and inter&shy;faces are and reflect on their impact and meaning for our society. Other topics of interests include <em>post-digital media</em>, <em>privacy</em>, and <em>en&shy;vi&shy;ron&shy;men&shy;tal issues</em>.</p>
-      <p>I prefer to work for organisations, foundations, universities, journalists, and activists with open source licenses. I also give talks and teach at universities.</p>
+      <p>I prefer to work for organisations, foundations, universities, journalists, and activists with open source licenses. I also give talks and teach at universities. In Berlin, I co-organise the <a href="https://datavis.berlin">DataVis Meetup</a>.</p>
       <p>I feel comfortable with common front-end, some back-end programming languages and a good range of graphic appli­cations. Most of my vi&shy;su&shy;al&shy;isa&shy;tions emerge from the combination of diverse tools ranging from data collection and analysis to custom vi&shy;su&shy;al&shy;isa&shy;tion.</p>
       <p>My private life is naturally influenced by my work and vice versa. Therefore, I am interested in all kinds of <em>design</em>, <em>culture</em> and <em>technology</em>, especially how we can use these tools to create a diverse, equal and fair world.</p>
       <p>Even though connected to my work, photography implies a break and recreation for me. Capturing something beautiful is a great combination of my love for aesthetics and the curiosity to explore foreign places.</p>
     </section>
-    <section>
+    <section v-if="process.env.lang === 'de'">
+      <h4>Links</h4>
+      <p>Da meine Arbeit und Projekte aus unterschiedlichen Disziplinen und Themenbereichen stammen, möchte ich diese auch an getrennten Orten halten:</p>
+      <ul class="list">
+        <li><a href="http://visualising.systems">visualising.systems</a><br />Meine letzten Daten-Visualisierungen und Informationsdesign Projekte</li>
+        <li><a href="https://away.photos">away.photos</a><br />Eine Sammlung meiner Fotografie Projekte</li>
+        <li><a href="http://karat.studio">karat.studio</a><br />Das Interface- und Informationsdesign Studio, das ich mit gegründet habe</p></li>
+        <li><a href="https://datavis.berlin">datavis.berlin</a><br />Eine Sammlung von Daten Visualisierung Links sowie die Seite des Berliner Daten Visualisierung Meetup</li>
+        <li><a href="http://zeto.io">zeto.io</a><br />Tools, die ich in den letzten Jahren gebaut habe</li>
+      </ul>
+    </section>
+    <section v-else>
       <h4>Links</h4>
       <p>As my work and the projects I work on cover different diciplines and topics I like to keep them in seperate places:</p>
       <ul class="list">
@@ -22,13 +44,13 @@
         <li><a href="http://zeto.io">zeto.io</a><br />Some online tools I build over the last years</li>
       </ul>
     </section>
-    <section>
-      <h4>Vita</h4>
-      <p>Jonas Parnow is a designer for in&shy;for&shy;ma&shy;tion and data. After his media design Bachelor of Arts from the <a href="https://www.rfh-koeln.de/">RFH Cologne</a> in 2012 he started at the info&shy;graphic agency <a href="https://infographics.group/">Infographics Group</a>. Besides that, he finished his design Master of Arts at the <a href="https://www.fh-potsdam.de/studieren/fachbereiche/design/">FH Potsdam</a>. Since 2017, he works as a part-time research associate at the <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a> and works in a EU-funded research project on the vi&shy;su&shy;al&shy;isa&shy;tion of climate change scenarios. He is also working at the inter&shy;face and in&shy;for&shy;ma&shy;tion design <a href="http://karat.studio">Studio Karat</a> that he co-founded. He also co-organises the <a href="https://datavis.berlin">data vi&shy;su&shy;al&shy;isa&shy;tion meetup</a> in Berlin.</p>
-    </section>
-    <section lang="de">
+    <section lang="de" v-if="process.env.lang === 'de'">
       <h4>Lebenslauf</h4>
       <p>Jonas Parnow ist Designer für Informationen und Daten. Nach dem Mediendesign Bachelor an der <a href="https://www.rfh-koeln.de/">RFH Köln</a> hat er 2012 bei der Infografik-Agentur <a href="https://infographics.group/">Infographics Group</a> angefangen. Parallel zu seiner Arbeit dort hat er 2015 an der <a href="https://www.fh-potsdam.de/studieren/fachbereiche/design/">FH Potsdam</a> einen Master in Interface Design abgeschlossen. Seit 2017 arbeitet er als wissen&shy;schaftlicher Mitarbeiter im <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a> und beschäftigt sich dort in einem EU-weiten Forschungs&shy;projekt mit der Visualisierung von Klimawandel-Szenarien. Schon während seiner Arbeit in der Agentur hat er <a href="http://karat.studio">Studio Karat</a> gegründet – ein Interface- und Informations&shy;design-Studio. Er co-organisiert außerdem das <a href="https://datavis.berlin">Daten Visualisierung Meetup</a> in Berlin.</p>
+    </section>
+    <section v-else>
+      <h4>Vita</h4>
+      <p>Jonas Parnow is a designer for in&shy;for&shy;ma&shy;tion and data. After his media design Bachelor of Arts from the <a href="https://www.rfh-koeln.de/">RFH Cologne</a> in 2012 he started at the info&shy;graphic agency <a href="https://infographics.group/">Infographics Group</a>. Besides that, he finished his design Master of Arts at the <a href="https://www.fh-potsdam.de/studieren/fachbereiche/design/">FH Potsdam</a>. Since 2017, he works as a part-time research associate at the <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a> and works in a EU-funded research project on the vi&shy;su&shy;al&shy;isa&shy;tion of climate change scenarios. He is also working at the inter&shy;face and in&shy;for&shy;ma&shy;tion design <a href="http://karat.studio">Studio Karat</a> that he co-founded. He also co-organises the <a href="https://datavis.berlin">data vi&shy;su&shy;al&shy;isa&shy;tion meetup</a> in Berlin.</p>
     </section>
   </div>
 </template>
