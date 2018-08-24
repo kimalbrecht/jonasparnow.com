62beb23b61b71eb2d7a6b756b3cd4ccc8ad921a0
Jonas <jonasparnow@gmail.com>
2018-08-24 10:20:23 +0200

    bugfix

--- assets/js/head.js
+++ assets/js/head.js
@@ -3,7 +3,7 @@
 module.exports = {
   links: function (config) {
     return [
-      { rel: 'license', href: base + config.page.imprint }
+      { rel: 'license', href: config.page.url + config.page.imprint }
     ]
   }
 }
