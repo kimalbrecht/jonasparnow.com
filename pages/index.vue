61b102668e84b77fdbff300e4f741776d4ec6cbf
Jonas <6gqrbj@posteo.org>
2019-03-24 11:02:12 +0100

    Update hyphens

--- pages/index.vue
+++ pages/index.vue
@@ -16,9 +16,9 @@
     </section>
     <section v-else>
       <h4>Introduction</h4>
-      <p>I’m interested in trans&shy;forming data and in&shy;for&shy;ma&shy;tion into <em>visual struc&shy;tures</em>. I have a critical and open under&shy;standing of what vi&shy;su&shy;al&shy;isa&shy;tion and inter&shy;faces are and reflect on their impact and meaning for our society. Other topics of interests include <em>post-digital media</em>, <em>privacy</em>, <em>inter&shy;sectional feminism</em>, and <em>en&shy;vi&shy;ron&shy;men&shy;tal issues</em>.</p>
+      <p>I’m interested in trans&shy;forming data and infor&shy;ma&shy;tion into <em>visual struc&shy;tures</em>. I have a critical and open under&shy;standing of what visu&shy;al&shy;isa&shy;tion and inter&shy;faces are and reflect on their impact and meaning for our society. Other topics of interests include <em>post-digital media</em>, <em>privacy</em>, <em>inter&shy;sectional feminism</em>, and <em>en&shy;vi&shy;ron&shy;men&shy;tal issues</em>.</p>
       <p>I prefer to work for organisations, foundations, universities, journalists, and activists with open source licenses. I also give talks and teach at universities. In Berlin, I co-organise the <a href="https://datavis.berlin">DataVis Meetup</a>.</p>
-      <p>I feel comfortable with common front-end, some back-end programming languages and a good range of graphic appli&shy;cations. Most of my vi&shy;su&shy;al&shy;isa&shy;tions emerge from the combination of diverse tools ranging from data collection and analysis to custom vi&shy;su&shy;al&shy;isa&shy;tion.</p>
+      <p>I feel comfortable with common front-end, some back-end programming languages and a good range of graphic appli&shy;cations. Most of my visu&shy;al&shy;isa&shy;tions emerge from the combination of diverse tools ranging from data collection and analysis to custom visu&shy;al&shy;isa&shy;tion.</p>
       <p>My private life is naturally influenced by my work and vice versa. Therefore, I am interested in all kinds of <em>design</em>, <em>culture</em> and <em>technology</em>, especially how we can use these tools to create a diverse, equal and fair world.</p>
       <p>Even though I had been working for some clients as a photographer I’m no longer taking photographs professionally. Nowadays, photography implies a break and recreation for me. Seeing the beauty of diversity, capturing moments that I shared with people or turning the camera down to hear the stories of the people surrounding me is all part of this journey.</p>
     </section>
@@ -37,16 +37,16 @@
       <h4>Links</h4>
       <p>As my work and the projects I work on cover different diciplines and topics I like to keep them in seperate places:</p>
       <ul class="list">
-        <li><a href="http://visualising.systems">visualising.systems</a><br />My recent data vi&shy;su&shy;al&shy;isa&shy;tion and in&shy;for&shy;ma&shy;tion design works</li>
+        <li><a href="http://visualising.systems">visualising.systems</a><br />My recent data visu&shy;al&shy;isa&shy;tion and infor&shy;ma&shy;tion design works</li>
         <li><a href="https://away.photos">away.photos</a><br />A collection of my favourite photography projects</li>
-        <li><a href="http://karat.studio">karat.studio</a><br />The inter&shy;face and in&shy;for&shy;ma&shy;tion studio I co-run</li>
-        <li><a href="https://datavis.berlin">datavis.berlin</a><br />A collection of data vi&shy;su&shy;al&shy;isa&shy;tion links I build for the data vis Meetup</li>
+        <li><a href="http://karat.studio">karat.studio</a><br />The inter&shy;face and infor&shy;ma&shy;tion studio I co-run</li>
+        <li><a href="https://datavis.berlin">datavis.berlin</a><br />A collection of data visu&shy;al&shy;isa&shy;tion links I build for the data vis Meetup</li>
         <li><a href="http://zeto.io">zeto.io</a><br />Some online tools I build over the last years</li>
       </ul>
     </section>
     <section>
       <h4>Vita</h4>
-      <p>Jonas Parnow is a designer for in&shy;for&shy;ma&shy;tion and data. After his media design Bachelor of Arts from the <a href="https://www.rfh-koeln.de/">RFH Cologne</a> in 2012 he started at the info&shy;graphic agency <a href="https://infographics.group/">Infographics Group</a>. Besides that, he finished his design Master of Arts at the <a href="https://www.fh-potsdam.de/studieren/fachbereiche/design/">FH Potsdam</a>. Since 2017, he works as a part-time research associate at the <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a> in the EU-funded <a href="http://senses-project.org/">Senses research project</a> on the vi&shy;su&shy;al&shy;isa&shy;tion of climate change scenarios. He is also working at the inter&shy;face and in&shy;for&shy;ma&shy;tion design <a href="http://karat.studio">Studio Karat</a> that he co-founded. Besides, he teaches data journalism at the <a href="https://journalistik.tu-dortmund.de/">TU Dortmund</a> and worked for <a href="https://www.zeit.de/index">Zeit Online</a>. He also co-organises the <a href="https://datavis.berlin">data vi&shy;su&shy;al&shy;isa&shy;tion meetup</a> in Berlin.</p>
+      <p>Jonas Parnow is a designer for infor&shy;ma&shy;tion and data. After his media design Bachelor of Arts from the <a href="https://www.rfh-koeln.de/">RFH Cologne</a> in 2012 he started at the info&shy;graphic agency <a href="https://infographics.group/">Infographics Group</a>. Besides that, he finished his design Master of Arts at the <a href="https://www.fh-potsdam.de/studieren/fachbereiche/design/">FH Potsdam</a>. Since 2017, he works as a part-time research associate at the <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a> in the EU-funded <a href="http://senses-project.org/">Senses research project</a> on the visu&shy;al&shy;isa&shy;tion of climate change scenarios. He is also working at the inter&shy;face and infor&shy;ma&shy;tion design <a href="http://karat.studio">Studio Karat</a> that he co-founded. Besides, he teaches data journalism at the <a href="https://journalistik.tu-dortmund.de/">TU Dortmund</a> and worked for <a href="https://www.zeit.de/index">Zeit Online</a>. He also co-organises the <a href="https://datavis.berlin">data visu&shy;al&shy;isa&shy;tion meetup</a> in Berlin.</p>
       <p>Jonas Parnow identifies as white, cis-male from Germany. Pronouns are <a href="https://www.mypronouns.org/he-him">he/him</a>.</p>
     </section>
     <section lang="de">
